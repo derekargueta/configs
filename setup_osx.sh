ea9f911ff50596f57b072153ff6e798367935643
Derek Argueta <darguetap@gmail.com>
2016-06-22 15:00:09 -0700

    Create setup_osx.sh

---
+++ setup_osx.sh
@@ -0,0 +1,36 @@
+##### install homebrew #####
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+##### now dem homebrew packages #####
+brew install ant
+brew install avrdude
+brew install boost
+brew install docker
+brew install gcc
+brew install git
+brew install go
+brew install gradle
+brew install htop
+brew install lua
+brew install maven
+brew install mongodb
+brew install mycli
+brew install mysql
+brew install nginx
+brew install nmap
+brew install nvm
+brew install osquery
+brew install python
+brew install python3
+brew install redis
+brew install rocksdb
+brew install rust
+brew install scala
+brew install snappy
+brew install sqlite
+brew install thrift
+brew install tomcat
+brew install tor
+brew install wget
+brew install wifi-password
+brew install youtube-dl
