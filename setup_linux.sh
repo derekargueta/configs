211120c3075fb68364efae3c5245df9544a6e690
Derek Argueta <darguetap@gmail.com>
2017-01-25 14:20:32 -0700

    Create setup_linux.sh

---
+++ setup_linux.sh
@@ -0,0 +1,20 @@
+
+sudo apt-get install build-essential
+sudo apt-get install nginx
+
+# tools
+sudo apt-get install git
+sudo apt-get install valgrind
+sudo apt-get install llvm clang-3.8
+sudo apt-get install gradle
+
+# languages
+sudo apt-get install lua5.2 lua5.3
+sudo apt-get install haskell-platform
+sudo apt-get install erlang
+sudo apt-get install mit-scheme
+# Golang should be installed from downloadable binary
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+nvm install 7.4
