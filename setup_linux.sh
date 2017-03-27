4c52a2a405f6d760d9f8ea17a3b571d1772b9c2e
Derek Argueta <darguetap@gmail.com>
2017-03-27 16:31:44 -0600

    Nginx vim highlighting

--- setup_linux.sh
+++ setup_linux.sh
@@ -7,6 +7,7 @@ sudo apt-get install git
 sudo apt-get install valgrind
 sudo apt-get install llvm clang-3.8
 sudo apt-get install gradle
+sudo apt-get install wget
 
 # libraries
 sudo apt-get install libssl-dev
@@ -21,3 +22,12 @@ sudo apt-get install mit-scheme
 # Install NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 nvm install 7.4
+
+# nginx Vim highlighting
+# http://www.vim.org/scripts/script.php?script_id=1886
+mkdir -p ~/.vim
+mkdir p ~/.vim/syntax
+wget http://www.vim.org/scripts/download_script.php?src_id=19394
+mv download_script.php?src_id=19394 nginx.vim
+cd ..
+echo "au BufRead,BufNewFile /etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif" >> filetype.vim
